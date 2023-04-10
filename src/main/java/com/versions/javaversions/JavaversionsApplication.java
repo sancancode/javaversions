2f8531b7d7fb0cf145f7aea74359b34e1be486c9
Sanjeeva Nemani <you@example.com>
2023-04-10 15:01:36 -0500

    added config

--- src/main/java/com/versions/javaversions/JavaversionsApplication.java
+++ src/main/java/com/versions/javaversions/JavaversionsApplication.java
@@ -2,10 +2,17 @@ package com.versions.javaversions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaversionsApplication {
+public class JavaversionsApplication  extends SpringBootServletInitializer {
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JavaversionsApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(JavaversionsApplication.class, args);
     }
