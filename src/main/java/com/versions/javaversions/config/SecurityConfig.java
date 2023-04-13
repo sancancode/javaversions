c9b6480aba11528e9abc20da439b8cc413721e89
Sanjeeva Nemani <you@example.com>
2023-04-12 21:09:59 -0500

    updating for heroku

--- src/main/java/com/versions/javaversions/config/SecurityConfig.java
+++ src/main/java/com/versions/javaversions/config/SecurityConfig.java
@@ -16,7 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/versionchanges","/","/home").authenticated()
                 .anyRequest().permitAll()
